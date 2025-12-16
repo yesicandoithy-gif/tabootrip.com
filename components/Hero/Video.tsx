fd1ed61c12a7717554c2757e2c9e5488e8bfe665
yesicandoithy-gif <yesicandoithy@gmail.com>
2025-12-16 21:58:21 +0700

    Fix hero video with smaller hot air balloon HD version

--- components/Hero/Video.tsx
+++ components/Hero/Video.tsx
@@ -6,9 +6,16 @@ const Video = () => {
       loop
       autoPlay
       muted
-      src="https://videos.pexels.com/video-files/5969958/5969958-uhd_2560_1440_30fps.mp4"
+      playsInline  // 加这个，手机端更好播放
       className="object-cover w-full h-[60vh] lg:h-[80vh]"
-    />
+    >
+      <source 
+        src="https://videos.pexels.com/video-files/10745869/10745869-hd_1920_1080_30fps.mp4" 
+        type="video/mp4" 
+      />
+      {/* 备用：如果不播放，显示原海滩视频 */}
+      <source src="/videos/palm_trees.mp4" type="video/mp4" />
+    </video>
   );
 };
 
